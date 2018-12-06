1661bb728be9668a70c30f738395b77fd18c5bd4
Ryan Kendrick <rpkendrick@gmail.com>
2018-12-06 23:05:09 +0000

    user feature progress

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,14 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
   end
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -71,4 +79,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
